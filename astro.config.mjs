8c19eefedf1322021eced8b3c0aa57a5f58390ef
Esteban Jimenez Valdez <wiwiwixd732@gmail.com>
2023-09-30 14:15:00 -0700

    MAJOR: astro-intro-project finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify/functions";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
+  site: "https://example.com",
+  integrations: [preact()]
 });
