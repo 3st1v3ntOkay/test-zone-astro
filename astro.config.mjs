6c65bc1a53401f7eeb2f61492f5c81791ff98ddf
Esteban Jimenez Valdez <wiwiwixd732@gmail.com>
2023-10-08 10:45:50 -0700

    feat: astro.config.mjs modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
-
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  site: "https://example.com",
-  integrations: [preact()]
+  site: "https://astrotestme.netlify.app/",
+  integrations: [preact()],
 });
